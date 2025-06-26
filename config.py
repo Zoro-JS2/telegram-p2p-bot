5306a5b421023398091905318303eb23cb4b06cd
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 <dema2000g@gmail.com>
2025-06-26 11:01:28 +0300

    Initial commit with .gitignore and .env support

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-BOT_TOKEN = "7651180913:AAGLmkLiplAxfRGWjjk5cFPDnlP4moYPtZ4"
-ADMIN_IDS = [450978014]
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+
+DATABASE_URL = os.getenv("DATABASE_URL")
